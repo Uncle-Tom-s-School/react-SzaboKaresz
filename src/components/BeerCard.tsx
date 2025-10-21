0563c09a5c511fbb32f1271e11397cce74b72060
Karoly <you@example.com>
2025-10-21 09:19:08 +0200

    Készen van

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,22 @@
-const BeerCard = () => {
+export type Beer = {
+  image: string
+  name: string
+  abv: string
+  volume: string
+  price: number
+  available: boolean
+}
+
+const BeerCard = (props: Beer) => {
+  const { image, name, abv, volume, price, available } = props
+
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+      <img src={image} alt={name} />
+      <h2>{name} {abv}%, {volume && <p>{volume}</p>}</h2>
+      {price !== undefined && <p>{price} Ft</p>}
+      <p>{available ? <p className='available'>Raktáron</p> : <p className='notAvailable'>Nincs raktáron</p>}</p>
+    </div>
   )
 }
 
